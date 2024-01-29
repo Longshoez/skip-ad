61fbd1400e5a609ec28ac7626ce5e48c3dd9dccc
Gabriel <gabrielalejandro.br99@gmail.com>
2024-01-29 10:34:27 -0600

    It seems stopping the video playback is the fastest way to skip the ad.

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-console.log("gets here")
+//code to inject to website
 
-let vp = document.querySelector('.video-stream')
+const myButton = document.createElement("button");
+myButton.textContent = "Skip ad"; 
 
-const stopSkip = () => {
+// Style the button using inline styles
+myButton.classList.add("adButtonSkip");
+
+// Add the button to the body of the document
+document.body.appendChild(myButton);
+
+// let vp = document.querySelector('.video-stream')
+										//find the streaming website skip ad button, in this example im using yt
+let adBanner = document.querySelector('.ytp-ad-player-overlay-flyout-cta')
+//ytp-ad-skip-button-modern ytp-button
+
+const stopSkip = (vp) => {
 	let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
 	vp.muted = false
 	vp.playbackRate = 1
-	if (skipBtn !== null) {
-		skipBtn.click()
+	if (skipBtn) {
+		skipBtn.click()	
+	}else{
+		console.log("couldnt find skip button")
 	}
 }
 
 const skipAd = () => {
-	console.log("attempting to skip add");
-	let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
-	vp.muted = true
-	vp.playbackRate = 16
-	setTimeout(function() {
-		stopSkip()
-	}, 1000);
-}
+	let vp = document.querySelector('.video-stream')
+	// let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
+	// let skipBtn2 = document.getElementsByClassName('ytp-ad-skip-button-modern')[0]
+	vp.muted = true 
+	vp.playbackRate = 0
+	// vp.playbackRate = 16
+	setTimeout(() => {
+		vp.muted = false
+		vp.playbackRate = 1
+		stopSkip(vp)
+	}, 350);
+} 
+
+myButton.setAttribute("onclick", "skipAd()")
+
+//const videoPlayer = document.querySelector(".ytd-player")
+
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode == 96 || event.keyCode == 92) {
+    skipAd()
+  }
+  
+  //190 220
+});
