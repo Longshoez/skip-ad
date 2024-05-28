91b1ee9b8bd2ae771c7ef52ccd46175531598511
Gabriel <gabrielalejandro.br99@gmail.com>
2024-05-28 12:18:43 -0600

    improved the keydown conditional

--- watch-it-faster.js
+++ watch-it-faster.js
@@ -3,7 +3,7 @@ const Custom_Styles = css => document.head.appendChild(document.createElement('s
 Custom_Styles(`@import url('./watch-it-faster.css');`)
 
 const myButton = document.createElement("button");
-myButton.textContent = "Skip ad"; 
+myButton.textContent = "Skip ad";
 
 // Style the button using inline styles
 myButton.classList.add("adButtonSkip");
@@ -16,47 +16,55 @@ let adBanner = document.querySelector('.ytp-ad-player-overlay-flyout-cta')
 //ytp-ad-skip-button-modern ytp-button
 
 const stopSkip = (vp) => {
-	// let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
-	let skipBtn = document.querySelector('.ytp-skip-ad-button')
-	vp.muted = false
-	vp.playbackRate = 1
-	if (skipBtn) {
-		skipBtn.click()	
-	}else{
-		console.log("couldnt find skip button")
-	}
+  // let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
+  let skipBtn = document.querySelector('.ytp-skip-ad-button')
+  vp.muted = false
+  vp.playbackRate = 1
+  if (skipBtn) {
+    skipBtn.click()
+  } else {
+    console.log("couldnt find skip button")
+  }
 }
 
 const skipAd = () => {
-	let vp = document.querySelector('.video-stream')
-	// let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
-	// let skipBtn2 = document.getElementsByClassName('ytp-ad-skip-button-modern')[0]
-	vp.muted = true
-	// vp.playbackRate = 16
-	//it seems stopping the playback is the fastest way to skip the ad. 
-	//yes it was, but sometimes youtube ads (the test subject). doesnt let you skip it, sometimes
-	//it forces you to play the whole thing. thats why the speed up method came back
-	vp.playbackRate = 16
-	setTimeout(() => {
-		vp.muted = false
-		vp.playbackRate = 1
-		stopSkip(vp)
-	}, 350);
-} 
+  let vp = document.querySelector('.video-stream')
+  // let skipBtn = document.querySelector('.ytp-ad-skip-button-modern')
+  // let skipBtn2 = document.getElementsByClassName('ytp-ad-skip-button-modern')[0]
+  vp.muted = true
+  // vp.playbackRate = 16 
+  //it seems stopping the playback is the fastest way to skip the ad. 
+  //yes it was, but sometimes youtube ads (the test subject). doesnt let you skip it, sometimes
+  //it forces you to play the whole thing. thats why the speed up method came back
+  vp.playbackRate = 16
+  setTimeout(() => {
+    vp.muted = false
+    vp.playbackRate = 1
+    stopSkip(vp)
+  }, 350);
+}
 
 myButton.setAttribute("onclick", "skipAd()")
 
 //const videoPlayer = document.querySelector(".ytd-player")
 
 document.addEventListener("keypress", function(event) {
-  if (
-      event.keyCode == 96 || 
-      event.keyCode == 92 || 
-      event.keyCode == 221 ||
-      event.keyCode == 45  
-  ) {
+  // if (
+  //   event.keyCode == 96 ||
+  //   event.keyCode == 92 ||
+  //   event.keyCode == 221 ||
+  //   event.keyCode == 124 ||
+  //   event.keyCode == 45
+  // ) {
+  //   skipAd()
+  //  not deprecated alternative 
+  // if (event.key == '`' || '|') {
+  //   skipAd()
+  // }
+  // supossedlt better alternative 
+  if (['`', '|', '}', '\\'].includes(event.key))
     skipAd()
-  }
+
 });
 
 // document.addEventListener("keypress", function(event) {
